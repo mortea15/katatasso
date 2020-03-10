d68ba1b230d8eac3c91141c267c28dedd7a86086
mortea15 <m.amundsen@pm.me>
2020-03-10 10:53:37 +0100

    constants

---
+++ katatasso/helpers/const.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+CATEGORIES = {
+    0: 'legitimate',
+    1: 'spam',
+    2: 'phishing',
+    3: 'malware',
+    4: 'fraud',
+    5: 'unclassified'
+}
+
+FP_MODEL = os.getenv('CLF_MODEL_PATH', 'model.pkl')
+CLF_DICT_NUM = int(os.getenv('CLF_DICT_NUM', 5000))
+CLF_TRAININGDATA_PATH = os.getenv('CLF_TRAININGDATA_PATH', 'trainingdata/emails/')
