670f1ef66d2e73db87a89af2fdb3b9e9c420a5e4
mortea15 <m.amundsen@pm.me>
2020-03-12 13:43:53 +0100

    option to toggle logging to file

--- katatasso/helpers/logger.py
+++ katatasso/helpers/logger.py
@@ -6,11 +6,7 @@ filename = 'katatasso'
 
 logFormatter = logging.Formatter('%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s')
 rootLogger = logging.getLogger()
-rootLogger.setLevel(30)
-
-fileHandler = logging.FileHandler(f'{filename}.log')
-fileHandler.setFormatter(logFormatter)
-rootLogger.addHandler(fileHandler)
+rootLogger.setLevel(50)
 
 consoleHandler = logging.StreamHandler()
 consoleHandler.setFormatter(logFormatter)
@@ -29,4 +25,9 @@ def increase_log_level():
         new_level = current - 10
         if new_level % 10 == 0 and new_level in range(10,51):
             rootLogger.setLevel(current - 10)
-            logging.info(f'Set log level to {levels.get(new_level)}')
+            print(f'Set log level to {levels.get(new_level)}')
+
+def log_to_file():
+    fileHandler = logging.FileHandler(f'{filename}.log')
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
