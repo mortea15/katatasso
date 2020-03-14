208f6040ed016a8e224b0d307557d8f023241698
mortea15 <m.amundsen@pm.me>
2020-03-14 20:23:59 +0100

    changed loglevel

--- katatasso/katatasso.py
+++ katatasso/katatasso.py
@@ -4,11 +4,11 @@ import os
 import sys
 
 from katatasso.helpers.logger import rootLogger as logger
-from katatasso.modules.classifier import classify
+from katatasso.modules.classifier import classify, classifyv2
 from katatasso.modules.trainer import train, trainv2
 
 try:
     import sklearn
 except ModuleNotFoundError:
-    logger.error(f'Module scikit-learn not found. Please install before proceeding.')
+    logger.critical(f'Module scikit-learn not found. Please install before proceeding.')
     sys.exit(2)
