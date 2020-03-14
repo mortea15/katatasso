e15d1142f3b2bac2ebc270a6b04099065183644c
mortea15 <m.amundsen@pm.me>
2020-03-14 20:24:58 +0100

    added choice between classifiers

--- katatasso/__main__.py
+++ katatasso/__main__.py
@@ -7,20 +7,21 @@ import sys
 import katatasso
 from katatasso.helpers.logger import increase_log_level, log_to_file
 from katatasso.helpers.logger import rootLogger as logger
-from katatasso.helpers.utils import prepare_result
+from katatasso.helpers.const import CATEGORIES
 
 current = os.path.realpath(os.path.dirname(__file__))
 APPNAME = 'katatasso'
 
 
 INDENT = '  '
-HELPMSG = f'''usage: {APPNAME} (-f INPUT_FILE | -s) [-t] [-c] [-d FORMAT] [-o OUTPUT_FILE] [-v] [-l]
+HELPMSG = f'''usage: {APPNAME} (-f INPUT_FILE | -s) [-t (v1 | v2)] [-c] [-d FORMAT] [-o OUTPUT_FILE] [-v] [-l]
 
     {INDENT * 1}-f, --infile        {INDENT * 2}Extract entities from file
     {INDENT * 1}-s, --stdin         {INDENT * 2}Extract entities from STDIN
 
-    {INDENT * 1}-t, --train         {INDENT * 2}Train and create a model for classification
-    {INDENT * 1}-c, --classify      {INDENT * 2}Classify the text
+    {INDENT * 1}-t, --train         {INDENT * 2}Train and create a model for classification. Specify either `v1` or `v2` as arg.
+    {INDENT * 1}-c, --classify      {INDENT * 2}Classify the text. Specify either `v1` or `v2` as arg,
+                              depending on what mode was used for training.
 
     {INDENT * 1}-o, --outfile       {INDENT * 2}Output results to this file
     {INDENT * 1}-d, --format        {INDENT * 2}Output results as this format.
@@ -36,17 +37,17 @@ def main():
     TEXT = None
     CONFIG = {}
 
-    result = None
+    result = {}
 
     if len(sys.argv) < 2:
         print(HELPMSG)
-        logger.error('No input specified')
+        logger.critical('No input specified')
         sys.exit(2)
     
     argv = sys.argv[1:]
 
     try:
-        opts, args = getopt.getopt(argv, 'hf:st:co:d:v', ['help', 'infile=', 'stdin', 'train=', 'classify', 'outfile=', 'format=', 'verbose'])
+        opts, args = getopt.getopt(argv, 'hf:st:c:o:d:v', ['help', 'infile=', 'stdin', 'train=', 'classify=', 'outfile=', 'format=', 'verbose'])
     except getopt.GetoptError:
         print(HELPMSG)
         sys.exit(2)
@@ -85,10 +86,10 @@ def main():
                 with open(file_path, 'r') as f:
                     TEXT = f.read()
             except FileNotFoundError:
-                logger.error(f'The specified file {file_path} does not exist.')
+                logger.critical(f'The specified file {file_path} does not exist.')
                 sys.exit(2)
             except Exception as e:
-                logger.error(f'An error occurred while reading the file `{file_path}`.')
+                logger.critical(f'An error occurred while reading the file `{file_path}`.')
                 logger.error(e)
                 sys.exit(2)
         elif opt in ('-s', '--stdin'):
@@ -96,6 +97,7 @@ def main():
                 logger.debug(f'Using input from STDIN')
                 TEXT = sys.stdin.read()
             except Exception as e:
+                logger.critical(f'An error occurred while reading from stdin.')
                 logger.error(e)
                 sys.exit(2)
         elif opt in ('-t', '--train'):
@@ -105,14 +107,21 @@ def main():
             elif arg == 'v2':
                 katatasso.trainv2()
             else:
-                logger.error(f'Please specify either `v1` or `v2`.\nE.g. `katatasso -t v2`')
+                logger.critical(f'Please specify either `v1` or `v2`. E.g. `katatasso -t v2`')
                 sys.exit(2)
         elif opt in ('-c', '--classify'):
             if TEXT:
                 logger.debug(f'ACTION: Classifying input')
-                result = katatasso.classify(TEXT)
+                if arg == 'v1':
+                    category, accuracy = katatasso.classify(TEXT)
+                elif arg == 'v2':
+                    category, accuracy = katatasso.classifyv2(TEXT)
+                else:
+                    logger.critical(f'Please specify either `v1` or `v2`. E.g. `katatasso -c v2`')
+                    sys.exit(2)
+                result = { 'category': category, 'accuracy': accuracy, 'alias': CATEGORIES.get(category) }
             else:
-                logger.error(f'Missing input (specify using -f or -s)')
+                logger.critical(f'Missing input (specify using -f or -s)')
                 sys.exit(2)
         elif opt in ('-o', '--outfile'):
             logger.debug(f'CONFIG: Setting output file to {arg}')
@@ -122,28 +131,28 @@ def main():
                 logger.debug(f'CONFIG: Setting output file format to {arg}')
                 CONFIG['format'] = arg
             else:
-                logger.error('Invalid format. Must be one of [plain, json]')
+                logger.critical('Invalid format. Must be one of [plain, json]')
                 sys.exit(2)
         
     if result:
-        output = prepare_result(result)
-
-        outfile = CONFIG.get('outfile')
         outformat = CONFIG.get('format')
+        outfile = CONFIG.get('outfile')
         if outfile:
             ext = 'json' if outformat == 'json' else 'txt'
             fname = f'{outfile}.{ext}'
             if outformat == 'plain':
+                result['accuracy'] = int(result.get('accuracy'))
+                print(result)
                 with open(fname, 'w') as f:
-                    f.write('\n'.join(output))
+                    f.write('\n'.join(list(result.values())))
             elif outformat == 'json':
                 import json
                 with open(fname, 'w', encoding='utf-8') as f:
-                    json.dump(output, f, ensure_ascii=False, indent=4)
-            logger.info(f'Results saved to file `{fname}`')
+                    json.dump(result, f, ensure_ascii=False, indent=4)
+            logger.debug(f'Results saved to file `{fname}`')
             sys.exit(0)
         else:
-            print(output)
+            print(result)
             sys.exit(0)
 
 
