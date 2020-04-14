aa26126b9465a3913c3e32b6f9620472ace31946
mortea15 <m.amundsen@pm.me>
2020-04-14 11:34:06 +0200

    automatically create db with tags based on dir structure

---
+++ katatasso/helpers/autotagger.py
@@ -0,0 +1,74 @@
+import os
+import sqlite3
+import sys
+
+from katatasso.helpers.const import CATEGORIES, DBFILE, CLF_TRAININGDATA_PATH
+from katatasso.helpers.extraction import get_file_paths
+
+DATAPATH = CLF_TRAININGDATA_PATH
+phishing_dir = DATAPATH + 'phishing/'
+spam_dir = DATAPATH + 'spam/'
+legit_dir = DATAPATH + 'legitimate/'
+emails = []
+
+def load_emails():
+    legit = [ (legit_dir + fname, 0) for fname in os.listdir(legit_dir) ]
+    spam = [ (spam_dir + fname, 1) for fname in os.listdir(spam_dir) ]
+    phish = [ (phishing_dir + fname, 2) for fname in os.listdir(phishing_dir) ]
+
+    emails = legit + spam + phish
+
+    print(f'''Loaded {len(emails)} emails
+    => Legit:       {len(legit)}
+    => Spam:        {len(spam)}
+    => Phishing:    {len(phish)}
+    ''')
+
+    return emails
+
+def create_conn():
+    return sqlite3.connect(DBFILE)
+
+def init_db():
+    conn = create_conn()
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS tags (id INTEGER PRIMARY KEY AUTOINCREMENT, filepath TEXT NOT NULL, tag INTEGER)')
+    conn.commit()
+    conn.close()
+
+def tag():
+    conn = create_conn()
+    c = conn.cursor()
+    tags = load_emails()
+    c.executemany('INSERT INTO tags (filepath, tag) VALUES (?,?)', tags)
+    conn.commit()
+    conn.close()
+
+def count():
+    conn = create_conn()
+    c = conn.cursor()
+    c.execute('SELECT COUNT(*) FROM tags')
+    total = c.fetchone()[0]
+    c.execute('SELECT COUNT(*) FROM tags WHERE tag=?', (0,))
+    legit = c.fetchone()[0]
+    c.execute('SELECT COUNT(*) FROM tags WHERE tag=?', (1,))
+    spam = c.fetchone()[0]
+    c.execute('SELECT COUNT(*) FROM tags WHERE tag=?', (2,))
+    phish = c.fetchone()[0]
+    
+    print(f'''DB: {total} emails
+    => Legit:       {legit}
+    => Spam:        {spam}
+    => Phishing:    {phish}
+    ''')
+
+
+def main():
+    if not os.path.isfile(DBFILE):
+        print('DB not present. Creating..')
+        init_db()
+    tag()
+    count()
+
+if __name__ == '__main__':
+    main()
