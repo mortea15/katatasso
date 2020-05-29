eabb5602c8942f3f2763e69a9edb2824b44a7ee2
mortea15 <m.amundsen@pm.me>
2020-05-29 19:00:39 +0200

    fixes to object saving

--- katatasso/helpers/utils.py
+++ katatasso/helpers/utils.py
@@ -6,7 +6,7 @@ import os
 
 import tqdm
 
-from katatasso.helpers.const import FP_MODEL
+from katatasso.helpers.const import FN_MODEL
 from katatasso.helpers.logger import rootLogger as logger
 
 # Force the progress bar to be displayed regardless
@@ -21,7 +21,7 @@ def progress_bar(it):
         return it
 
 
-def __save_obj(obj, filepath):
+def save_obj(obj, filepath):
     try:
         with open(filepath, 'wb') as f:
             pickle.dump(obj, f)
@@ -31,7 +31,7 @@ def __save_obj(obj, filepath):
         logger.error(e)
 
 
-def __load_obj(filepath):
+def load_obj(filepath):
     try:
         with open(filepath, 'rb') as f:
             obj = pickle.load(f, encoding='latin1')
@@ -46,25 +46,21 @@ def __load_obj(filepath):
         sys.exit(2)
 
 
-def save_model(model):
-    __save_obj(model, FP_MODEL)
+def save_model(model, version='v2', algo='mnb'):
+    fname = f'{FN_MODEL}{version}-{algo}.p'
+    save_obj(model, fname)
 
 
-def load_model():
-    return __load_obj(FP_MODEL)
+def load_model(version='v2', algo='mnb'):
+    fname = f'{FN_MODEL}{version}-{algo}.p'
+    return load_obj(fname)
 
 
-def save_y_test(y_test):
-    __save_obj(y_test, f'y_test-{FP_MODEL}')
+def save_vectorizer(vectorizer, algo='mnb'):
+    fn = f'vectorizer_v2-{algo}.p'
+    save_obj(vectorizer, fn)
 
 
-def load_y_test():
-    return __load_obj(f'y_test-{FP_MODEL}')
-
-
-def save_vectorizer(vectorizer):
-    __save_obj(vectorizer, f'vectorizer-{FP_MODEL}')
-
-
-def load_vectorizer():
-    return __load_obj(f'vectorizer-{FP_MODEL}')
+def load_vectorizer(algo='mnb'):
+    fn = f'vectorizer_v2-{algo}.p'
+    return load_obj(fn)
